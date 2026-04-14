7709d2ae5030578a6b059f675c4b163a50d1f4e2
Achyutha2527 <achyuthagottumukkala@gmail.com>
2026-04-14 22:46:03 +0530

    Replace Toaster with Sonner component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes, Navigate, useLocation } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AuthProvider, useAuth } from "@/contexts/AuthContext";
 import BottomNav from "@/components/BottomNav";
@@ -59,7 +58,7 @@ function AppRoutes() {
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
+      
       <Sonner />
       <BrowserRouter>
         <AuthProvider>
